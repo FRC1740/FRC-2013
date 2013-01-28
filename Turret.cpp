a86b85e1eeacc6ed5ec472165f29abd63395fac7
frc1740 <frc1740@04f7d4cd-9f67-443a-8a3e-7a327d2c3081>
2013-01-28 20:25:48 +0000

    CRE: Copied spike code into Turret.cpp for development/testing

--- Turret.cpp
+++ Turret.cpp
@@ -10,12 +10,17 @@ public:
 		spike(1)
 	{
 	}
-	void powerChange(bool state) {
+	void cycle_linear_actuator(bool state) {
 
-		printf("testing: class spike, function powerChange");
-		spike.Set(Relay.Value.kForward);
-//		Wait(5);
-		spike.Set(Relay.Value.kReverse);
-//		Wait(5);
+		printf("testing: class Turret, function cycle");
+		int foo = spike.Get();
+		printf("Turret: spike status: %d\n", foo);
+		// spike.Set(Relay.Value.kForward);
+		printf("Turret: wait 5 sec...\n");
+		Wait(5);
+		// spike.Set(Relay.Value.kReverse);
+		printf("Turret: Wait 5 more sec...\n");
+		Wait(5);
+		printf("Turret: Exiting!\n");
 	}
 };
