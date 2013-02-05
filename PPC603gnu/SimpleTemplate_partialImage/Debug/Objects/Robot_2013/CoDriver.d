af0e69822e970cb779c3987a2b24286b2577d279
frc1740 <frc1740@04f7d4cd-9f67-443a-8a3e-7a327d2c3081>
2013-02-05 22:14:55 +0000

    dang you estabrooks!!!!!!!!!!!!!!!!!!!!!!! you broke it :P

---
+++ PPC603gnu/SimpleTemplate_partialImage/Debug/Objects/Robot_2013/CoDriver.d
@@ -0,0 +1,2 @@
+SimpleTemplate_partialImage/Debug/Objects/Robot_2013/CoDriver.o:  \
+ C:/Users/hjax/Desktop/robo/Robot_2013/CoDriver.cpp
---
+++ PPC603gnu/SimpleTemplate_partialImage/Debug/Objects/VisionSample2013/test.d
@@ -0,0 +1,2 @@
+SimpleTemplate_partialImage/Debug/Objects/VisionSample2013/test.o:  \
+ C:/Users/Ledyard/Desktop/WindRiverprojects/VisionSample2013/test.cpp
