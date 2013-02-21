eb68d6fa452b5138eef688f1825f26d5df728b69
frc1740 <frc1740@04f7d4cd-9f67-443a-8a3e-7a327d2c3081>
2013-02-21 04:15:13 +0000

    KTK: commented the main file (yay), added a todo list, i need to print the could out so i can read it

--- Robot_2013.cpp
+++ Robot_2013.cpp
@@ -8,45 +8,41 @@
 #include "support.h"
 
 /**
- * Sample program to use NIVision to find rectangles in the scene that are illuminated
- * by a LED ring light (similar to the model from FIRSTChoice). The camera sensitivity
- * is set very low so as to only show light sources and remove any distracting parts
- * of the image.
- * 
- * The CriteriaCollection is the set of criteria that is used to filter the set of
- * rectangles that are detected. In this example we're looking for rectangles with
- * a minimum width of 30 pixels and maximum of 400 pixels.
- * 
- * The algorithm first does a color threshold operation that only takes objects in the
- * scene that have a bright green color component. Then a convex hull operation fills 
- * all the rectangle outlines (even the partially occluded ones). Then a small object filter
- * removes small particles that might be caused by green reflection scattered from other 
- * parts of the scene. Finally all particles are scored on rectangularity, aspect ratio,
- * and hollowness to determine if they match the target.
- *
- * Look in the VisionImages directory inside the project that is created for the sample
- * images as well as the NI Vision Assistant file that contains the vision command
- * chain (open it with the Vision Assistant)
+ *	Team 1740
+ *	Ultimate Ascent Code
+ *	Programming Staff:
+ *	Brian Healy: Captain and Labview / TI84 guy
+ *	Kevin Konrad: Not-Captain and Python Guy / C++ guy
+ *	Charles Estabooks: Programming Mentor and C/C++ Guy
+ *	
+ *	Our code is free for the use of anyone that wants it :D
+ *	
+ *	
+ *	ToDo:
+ *	We should probably make a task deticated to updating all values in the driver station (even in autonomous), get rid of the bits scattered in the code
+ *	change where we are creating the tasks so that it starts all of them in initalization and just does a check to see if we are in teleop in the task perhaps?
+ *	
  */
 
 
 class Robot_2013 : public SimpleRobot
 {
 	
-	Scores *scores; //defined in CameraCode.h
-	CameraCode *cameraFunctions;
-	mainDriver *Driver1;
-	Lifter *spikeLifter;
-	Shooter *frisbeeShooter;
-	Loader *Sweeper;
-	coDriver *Driver2;
-	robotOut *mainOut;
-	DriverStationLCD *DsLCD;	
+	Scores *scores; //defined in CameraCode.h, needed for vision tracking 
+	CameraCode *cameraFunctions; // Moved all of the original camera processing code to here
+	mainDriver *Driver1; // The Functions that deal with the main driver and his two joysticks
+	Lifter *spikeLifter; // Controls our spike for the acutator, probably could make a more flexable one 
+	Shooter *frisbeeShooter; // Controls our two shooter motors
+	Loader *Sweeper; // Is responsible for the conveyor and and frisbee elevator
+	coDriver *Driver2; // The functions that deal with the co driver and he one joystick
+	robotOut *mainOut; // The function that we are using to process output
+	DriverStationLCD *DsLCD; // Used to put values in the User Messages box on the driver station
 
 	
 
 public:
 	Robot_2013(void) {
+		// These lines create a new object of the pointers that we created above
 		cameraFunctions = new CameraCode("amber");
 		Driver1 = new mainDriver;
 		Driver2 = new coDriver;
@@ -57,6 +53,7 @@ public:
 		DsLCD = DriverStationLCD::GetInstance();
 	}
 	~Robot_2013(void) {
+		// and these lines delete the functions for a nice cleanup 
 		delete cameraFunctions;
 		delete Driver1;
 		delete Driver2;
@@ -67,12 +64,14 @@ public:
 		delete DsLCD;
 	}
 	static int notifierTask(Robot_2013 *robot){
-		while (true){
-			for (int differentJoysticks = 1; differentJoysticks < 4; differentJoysticks++){
-				for (int count = 1; count < 12; count++){
-					char location [5];
-					bool buttonValue;
-					if (differentJoysticks == 1){
+		// This task manages putting joystick values on to the dashboard
+		while (true){  //  Being a task, we loop forever (or until Task::Stop() is used)
+			for (int differentJoysticks = 1; differentJoysticks < 4; differentJoysticks++){ // for each of our three joysticks
+				for (int count = 1; count < 12; count++){ // for each button on a joystick
+					char location [5]; // create a sting that has a buffer 5 characters long for brian cryptic button names
+					bool buttonValue; // create a bool that we will feed the current value of the button in to
+					// next we check what joystick we are on and feed that value into buttonValue
+					if (differentJoysticks == 1){ 
 						buttonValue = robot->Driver1->returnLeftJoystick(count);
 					}
 					if (differentJoysticks == 2){
@@ -81,10 +80,11 @@ public:
 					if (differentJoysticks == 3){
 						buttonValue = robot->Driver2->returnJoystick(count);
 					}
-					sprintf(location, "%d_%d", differentJoysticks, count);
-					SmartDashboard::PutBoolean(location, buttonValue);
+					sprintf(location, "%d_%d", differentJoysticks, count); // make a "joystick#_button#" string to appease brians dashboard
+					SmartDashboard::PutBoolean(location, buttonValue); // put the finished value on the dashboard
 				}
 			}
+			// the following lines cannot be put on the dashboard by my loop, so i threw them here
 			SmartDashboard::PutNumber("Y-Axis1", robot->Driver1->Lefty());
 			SmartDashboard::PutNumber("X-Axis1", robot->Driver1->Leftx());
 			SmartDashboard::PutNumber("Y-Axis2", robot->Driver1->Righty());
@@ -93,13 +93,14 @@ public:
 			SmartDashboard::PutNumber("Left Drive Motor", robot->Driver1->Righty());
 			SmartDashboard::PutNumber("Throttle2", robot->Driver1->rightThrottle());
 			SmartDashboard::PutNumber("Throttle1", robot->Driver1->leftThrottle());
-			Wait(.02);
+			Wait(.02);  // lets not starve the crio doing tasks
 		}
 		return 0;
 	}
 	static int lifterThread(Robot_2013 *robot){
 		while (true){
-			robot->Driver2->lifterCheck(robot->spikeLifter);
+			// this doesnt have to be in its own task anymore, however it isnt hurting anything
+			robot->Driver2->lifterCheck(robot->spikeLifter); // check if codriver has hit a button to control the actuator (spike)
 			// do not uncomment follow lines, it will hit the hard limit and damage the robot
 			//robot->Driver2->goToInchCheck(robot->spikeLifter);
 			//robot->Driver2->initalizeLifter(robot->spikeLifter);
@@ -109,16 +110,10 @@ public:
 	}
 	static int loaderThread(Robot_2013 *robot){
 		while (true){
-			robot->Driver2->conveyorCheck(robot->Sweeper);
-			robot->Sweeper->loaderSequence();
-			robot->Driver2->fireAtWill(robot->frisbeeShooter, robot->Sweeper);
+			robot->Driver2->conveyorCheck(robot->Sweeper); // does the driver want to start a conveyor
+			robot->Sweeper->loaderSequence();  // did we hit the limit switch to grab a frisbee?
+			robot->Driver2->fireAtWill(robot->frisbeeShooter, robot->Sweeper); // does the codriver want to fire?
 			Wait(.05);
-		}
-		return 0;
-	}
-	static int printy(Robot_2013 *robot){
-		while (true){
-			robot->mainOut->printDebug("testing testing 123", 1);
 		}
 		return 0;
 	}
@@ -129,13 +124,12 @@ public:
 	void Autonomous(void)
 	{	
 		mainOut->printDebug("Entering autonomous...\n", 1);
-		DsLCD->PrintfLine(DsLCD->kUser_Line1, "Entering autonomous mode");
+		DsLCD->PrintfLine(DsLCD->kUser_Line1, "Entering autonomous");  // print to the dashboard and console
 		DsLCD->UpdateLCD();
 		
 		while (IsAutonomous() && IsEnabled()) {
-			//cameraFunctions->targetImage();
-			Wait(.25);
-			fprintf(stderr, "linear actuator %3.2f\n", spikeLifter->getInches());
+			cameraFunctions->targetImage();  // find all of the targets
+			Wait(2);
 			SmartDashboard::PutNumber("Actuator Distance", spikeLifter->getInches());
 		}
 	}
@@ -145,7 +139,7 @@ public:
 	 */
 	void OperatorControl(void)
 	{
-		// Lets see if i can do something multithreaded
+		// Lets see if i can do something multithreaded, this needs to be organized badly
 		mainOut->printDebug("starting Teleop\n", 1);
 		char name[30];
 		sprintf(name, "lifterThread-%d", GetFPGATime());
@@ -190,12 +184,14 @@ public:
 
 	void Disabled(void)
 	{
+		// this code is run once whenever the robot is disabled, im not sure what would happen if we tried to loop
 		mainOut->printDebug("I\'m Disabled!!\n", 1); // This code runs whenever the robot is disabled, even if the printf buffer sometimes forgets to flush
 		DsLCD->PrintfLine(DsLCD->kUser_Line1, "Disabled");
 		DsLCD->UpdateLCD();
 
 	}
 	void RobotInit(void){
+		// Run once when the robot is turned on
 		mainOut->printDebug("I\'m Init`ed\n", 1);
 		DsLCD->PrintfLine(DsLCD->kUser_Line1, "Initalizing");
 		DsLCD->UpdateLCD();
@@ -203,6 +199,6 @@ public:
 
 };
 
-
+// This apparently tells the FRC thread to create an instance of our class and start calling its functions
 START_ROBOT_CLASS(Robot_2013);
 
